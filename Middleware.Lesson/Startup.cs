6ed918c01a13a3f8da478988b0fb40109b74bfad
smartshopperpay <bruno.ferreira.rocha@gmail.com>
2024-01-09 02:37:38 +0100

    #

--- Middleware.Lesson/Startup.cs
+++ Middleware.Lesson/Startup.cs
@@ -1,16 +1,11 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Middleware.Lesson.DB;
-using System.IO;
-using System.Net;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace Middleware.Lesson
 {
@@ -79,62 +74,7 @@ namespace Middleware.Lesson
     }
 
 }
-public class AuthenticationMiddleware
-{
-    private readonly RequestDelegate _next;
 
-    public AuthenticationMiddleware(RequestDelegate next)
-    {
-        _next = next;
-    }
-    public class Credentials
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-    }
-    public async Task InvokeAsync(HttpContext context)
-    {
-        // Leggi il body della richiesta
-        string body;
-        context.Request.EnableBuffering(); // Abilita la lettura del body più volte
-        using (var reader = new StreamReader(context.Request.Body))
-        {
-            body = await reader.ReadToEndAsync();
-            context.Request.Body.Position = 0; // Resetta la posizione del body per le letture successive
-        }
-
-        // Deserializza il body nella classe Credentials
-        Credentials credentials = null;
-        try
-        {
-            credentials = JsonSerializer.Deserialize<Credentials>(body);
-        }
-        catch (JsonException exx) // Gestisci errori di deserializzazione
-        {
-            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
-            await context.Response.WriteAsync("Body della richiesta non valido.");
-            return;
-        }
-
-        // Verifica le credenziali
-        if (credentials != null && IsValidUser(credentials))
-        {
-            await _next(context); // Continua con la pipeline se l'utente è valido  
-            await context.Response.WriteAsync("Accesso Concesso!");
-        }
-        else
-        {
-            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
-            await context.Response.WriteAsync("Accesso Negato");
-        }
-    }
-
-    private bool IsValidUser(Credentials credentials)
-    {
-        // Implementa la logica di verifica dell'utente
-        return credentials.Username == "bruno" && credentials.Password == "myPassword";
-    }
-}
 
 
 
